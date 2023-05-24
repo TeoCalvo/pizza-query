61b61c71bf0e74802cb0a69ec4f1ee2907c96c9f
renato.pacheco.r2@gmail.com <renato.pacheco.r2@gmail.com>
2023-05-24 23:49:23 +0000

    primeiros comandos

---
+++ dia-01/OlaMundo.sql
@@ -0,0 +1,15 @@
+-- Databricks notebook source
+SELECT 'OLA MUNDO'
+
+-- COMMAND ----------
+
+SELECT *                        -- selecione todas as colunas
+FROM silver.pizza_query.pedido  -- da tabela silver.pizza_query.pedido
+
+-- COMMAND ----------
+
+SELECT 'BOA NOITE'
+
+-- COMMAND ----------
+
+
