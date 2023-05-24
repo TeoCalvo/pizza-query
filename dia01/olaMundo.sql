006863447c37901444e473327d007c400e9a627a
jeancnds@gmail.com <jeancnds@gmail.com>
2023-05-24 23:48:12 +0000

    Olá mundo do primeiro dia
    Construção da primeira consulta no treinamento

---
+++ dia01/olaMundo.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+SELECT 'Olá, mundo!'
+
+-- COMMAND ----------
+
+SELECT * --selecione todas as colunas
+FROM silver.pizza_query.pedido --da tabela silver.pizza_query.pedido
