89442e22e915fb6e3765b852f503f5376c2d20ce
KyriaSkotadi <KyriaSkotadi>
2023-05-24 23:48:09 +0000

    comandos aprendidos no primeiro dia

---
+++ note-01.sql
@@ -0,0 +1,16 @@
+-- Databricks notebook source
+SELECT 'HELLO WORLD!' AS primComando
+
+-- COMMAND ----------
+
+SELECT 21 * 2 AS totalExpresssao
+
+-- COMMAND ----------
+
+SELECT 'OLÁ MUNDO!' AS  primComando,
+        21 * 2 totalExpressao
+
+-- COMMAND ----------
+
+SELECT    --selecione todas as colunas
+FROM silver.pizza_query.pedido   --da tabela silver.pizza_query.pedido
