ac3849217d636e665f81df144b1e97d761228084
alisonascimento <alisonascimento>
2023-05-25 00:07:07 +0000

    changing code
    changing the code to visualize all the tables

---
+++ dia01/tabelas-curso.sql
@@ -0,0 +1,24 @@
+-- Databricks notebook source
+SELECT 'OLÁ MUNDO!'
+
+-- COMMAND ----------
+
+SELECT *                          
+FROM silver.pizza_query.pedido
+LIMIT 10
+
+-- COMMAND ----------
+
+SELECT *
+FROM silver.pizza_query.item_pedido
+LIMIT 10
+
+-- COMMAND ----------
+
+SELECT *
+FROM silver.pizza_query.produto
+LIMIT 10
+
+-- COMMAND ----------
+
+
