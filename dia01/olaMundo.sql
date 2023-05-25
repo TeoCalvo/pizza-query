abbc3609d259fd83ede88804920e7051d723bf22
yagogeddes@hotmail.com <yagogeddes@hotmail.com>
2023-05-25 13:32:53 +0000

    Ola Mundo

--- dia01/olaMundo.sql
+++ dia01/olaMundo.sql
@@ -5,3 +5,12 @@ SELECT 'OLÁ MUNDO!'
 
 SELECT *                          -- selecione todas as colunas
 FROM silver.pizza_query.pedido    -- da tabela silver.pizza_query.pedido
+
+
+
+-- COMMAND ----------
+
+SELECT *
+FROM silver.pizza_query.pedido
+WHERE pedido.descUF='Rio de Janeiro'
+
