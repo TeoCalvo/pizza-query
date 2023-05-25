94adbb170c7e3ddb9c0e686782240755e53e41b7
es.sakamoto@gmail.com <es.sakamoto@gmail.com>
2023-05-25 18:28:13 +0000

    add um qry do item pedido de teste

--- dia01/olaMundo.sql
+++ dia01/olaMundo.sql
@@ -5,3 +5,9 @@ SELECT 'OLÁ MUNDO!'
 
 SELECT *                          -- selecione todas as colunas
 FROM silver.pizza_query.pedido    -- da tabela silver.pizza_query.pedido
+
+-- COMMAND ----------
+
+select * 
+from silver.pizza_query.item_pedido ip
+where ip.idPedido = 1
