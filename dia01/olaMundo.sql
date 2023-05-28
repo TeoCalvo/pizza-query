c1c958a19a67e40a7a15c7af576b1e827481c28a
ntorracca@gmail.com <ntorracca@gmail.com>
2023-05-28 16:22:00 +0000

    Nova query adicionada

--- dia01/olaMundo.sql
+++ dia01/olaMundo.sql
@@ -5,3 +5,13 @@ SELECT 'OLÁ MUNDO!' ola_mundo, 21*2 produto
 
 SELECT *                          -- selecione todas as colunas
 FROM silver.pizza_query.pedido    -- da tabela silver.pizza_query.pedido
+
+-- COMMAND ----------
+
+SELECT *                          -- selecione todas as colunas
+FROM silver.pizza_query.pedido    -- da tabela silver.pizza_query.pedido
+where descUF='Rio de Janeiro' and flKetchup=0
+
+-- COMMAND ----------
+
+
